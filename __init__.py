5b503a6065c1de9f718f26d2c8a0180ecde73a40
nitish770 <nitishkmahto257@gmail.com>
2021-01-21 07:36:22 -0500

    add make_dirs function

---
+++ __init__.py
@@ -0,0 +1,13 @@
+__title__ = "reducer"
+__author__ = "Nitish K. Mahto"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021"
+
+from . import compress
+from .__main__ import Compress
+
+from . import youtube
+from . import udemy
+
+__all__ = ["down", "extract", "Compress"]
+
---
+++ __main__.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+print('compress imported')
+
+
+class Compress:
+
+    def __init__(self, remote, local):
+        self.remote = remote
+        self.top_dir = self.remote.split('/')[-1]
+        self.root = self.remote
+        self.local = os.path.join(local, self.top_dir)
+        self.make_dirs(self.remote)
+        # self.local = local
+        # self.deepest_fold = 0
+
+    def valid_unix_name(self, name):
+        return '"'+name+'"'
+
+    def make_dirs(self, folder, quitIfMainFolderExists=1):
+        os.chdir(folder)
+
+        if quitIfMainFolderExists and os.path.exists(folder.replace(self.root, self.local)):
+            print('exists', folder, (folder.replace(self.root, self.local)))
+            return
+
+        # print(folder)
+        for dirs in os.listdir(folder):
+            new_content = folder + '/' + dirs
+            if os.path.isdir(new_content):
+                self.make_dirs(new_content)
+                os.system('mkdir -p ' +
+                          self.valid_unix_name(new_content.replace(self.root, self.local)))
+
